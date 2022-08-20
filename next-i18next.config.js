268fcc733477995de438cbce81e999e04649f471
Leonardo Morini <leo.morini.nn@gmail.com>
2022-08-20 11:37:46 -0300

    [ main ] - fix vercel error build

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,11 @@
+const path = require('path')
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'pt'],
-    localePath: path.resolve('./public/locales')
+    ...(typeof window === undefined
+      ? { localePath: path.resolve('./public/locales') }
+      : {})
   },
 };
